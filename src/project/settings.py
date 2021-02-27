b908a49d10dda405ed992d0ef19009cedf3abfe4
Sergey <megasega@tut.by>
2021-02-27 13:38:54 +0300

    try to make work heroku

--- src/project/settings.py
+++ src/project/settings.py
@@ -28,7 +28,12 @@ SECRET_KEY = os.getenv("SECRET_KEY", settings.SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = settings.MODE_DEBUG
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "sergey-firstproject.herokuapp.com",
+    "localhost",
+    "127.0.0.1",
+]
+
 
 
 # Application definition
